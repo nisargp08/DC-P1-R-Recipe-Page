eb3632347ead1dd76f8372faa7f3c6413d3dcbb7
Nisarg Patel <nisargp42@gmail.com>
2020-09-12 03:28:54 -0400

    Mobile :: Layout :: Ready

--- tailwind.js
+++ tailwind.js
@@ -1,7 +1,28 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily : {
+          montserrat: ["Montserrat", "sans-serif"],
+          playfair: ["Playfair Display", "serif"],
+      },
+      borderRadius : {
+        'xl' : '0.75rem',
+      },
+      fontSize : {
+        'xxs' : '10px',
+        '2xxs' : '8px'
+      },
+      textColor : {
+        'gray-1' : '#333333',
+        'gray-2' : '#4F4F4F',
+        'gray-4' : '#BDBDBD',
+        'gray-5' : '#A9A9A9',
+      },
+      spacing : {
+        '2px' : '2px',
+      }
+    },
   },
   variants: {},
   plugins: [],
